b8a4972254d43b79640493be1b4125130668bcce
taehyunlim <taehyun.lim@gmail.com>
2018-10-09 22:18:39 -0700

    Refactored class components into functional (stateless) components AND directly passed on state change methods as props to child components

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,42 +1,34 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import Book from './Book';
 
-class SearchResult extends React.Component {
-
-  updateBook = (book, shelf, isNew) => {
-    this.props.onUpdateBook(book, shelf, isNew)
-  }
-
-  render() {
-    const { books, bookIdsByShelf } = this.props;
-    const shelves = Object.keys(bookIdsByShelf)
-    let hash = {}
-    shelves.forEach(shelf => {
-      bookIdsByShelf[shelf].forEach(id => {
-        hash = Object.assign({[id]: shelf}, hash)
-      })
+const SearchResult = (props) => {
+  const { books, bookIdsByShelf } = props;
+  const shelves = Object.keys(bookIdsByShelf)
+  let hash = {}
+  shelves.forEach(shelf => {
+    bookIdsByShelf[shelf].forEach(id => {
+      hash = Object.assign({[id]: shelf}, hash)
     })
-    const booksWithShelf = books.map(book => (
-      Object.assign({"shelf": (hash[book.id]) ? hash[book.id] : "none"}, book)
-    ))
+  })
+  const booksWithShelf = books.map(book => (
+    Object.assign({"shelf": (hash[book.id]) ? hash[book.id] : "none"}, book)
+  ))
 
-    return(
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {booksWithShelf.length > 0 && 
-            booksWithShelf.map((book) => (
-              <Book
-                key={book.id}
-                book={book}
-                shelf={book.shelf}
-                onUpdateBook={this.updateBook} />
-            ))
-          }
-        </ol>
-      </div>
-    )
-  } 
-}
+  return(
+    <div className="search-books-results">
+      <ol className="books-grid">
+        {booksWithShelf.length > 0 && 
+          booksWithShelf.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              shelf={book.shelf}
+              onUpdateBook={props.onUpdateBook} />
+          ))
+        }
+      </ol>
+    </div>
+  )
+} 
 
 export default SearchResult;
